1ced2235c238d30f8985ce8b8f5090a281dbfdf7
Robson Pinto <robson.pinto@aegea.com.br>
2021-09-01 09:04:50 -0300

    Lidando com cache do NextJS

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c1d28479fbeccd133344"; }
+/******/ 		__webpack_require__.h = function() { return "d95c329d43be2fc4242f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.86611a1f54d951e361b6.hot-update.js
+++ .next/static/webpack/webpack.86611a1f54d951e361b6.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "fddd0ab3d70f713b5543"; }
+/******/ 	__webpack_require__.h = function() { return "29dd28450ec55561b1a7"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
